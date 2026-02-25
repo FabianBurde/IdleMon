88e7ec58676b691c299829c349bc32f6234a4e57
fabian burde <Fabian.Burde@web.de>
2026-02-25 13:59:28 +0100

    fixed unit saving/loading. Saving UnitResource as Dictonary for now. VER 0.1 Stable

--- Data/unit.gd
+++ Data/unit.gd
@@ -10,17 +10,19 @@ var phys_def:float
 var magic_def:float
 var speed:float
 var attack_rate:float
+var star_value:int
 
 @export var base_stat_points:int
 
-func _init():
-	randomize()
-	unit_name = "New Unit"
-	health = 100.0
-	_set_random_stats(10, 50)
+#func _init():
+	#randomize()
+	#unit_name = "New Unit"
+	#health = 100.0
+	#_set_random_stats(10, 50)
 
 
 func _set_random_stats(min_stat:int, max_stat:int) -> void:
+	randomize()
 	phys_dmg = randi_range(min_stat, max_stat)
 	magic_dmg = randi_range(min_stat, max_stat)
 	phys_def = randi_range(min_stat, max_stat)
@@ -29,11 +31,13 @@ func _set_random_stats(min_stat:int, max_stat:int) -> void:
 	print("Generated Unit Stats: ")
 	print( phys_dmg, " ", magic_dmg, " ", phys_def, " ", magic_def, " ", speed)
 
+func _set_unit_texture(tex:Texture2D) -> void:
+	unit_tex = tex
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	base_stat_points = 100
-	_set_random_stats(10, base_stat_points)
+	#_set_random_stats(10, base_stat_points)
 	
 
 
